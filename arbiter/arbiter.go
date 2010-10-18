900dff50714515a05d566ac2a0efc131733a45c0
Maks Verver <maksverver@geocities.com>
2010-10-19 01:25:36 +0200

    Updated Makefile to include MO.c and updated a few other files.

--- arbiter/arbiter.go
+++ arbiter/arbiter.go
@@ -2,10 +2,44 @@ package main
 
 import (
 	// "dvonn"
+	//"exec"
 	"flag"
 	"fmt"
 	"os"
 )
+
+type Result struct {
+	player  [2]int    // 0-based player indices
+	discs   [2]int    // final number of discs
+	failed  [2]bool   // whether player failed
+	points  [2]int    // CodeCup-style points
+	time    [2]float  // total time taken
+}
+
+func runMatch(commands []string, players [2]int) : result Result {
+	result.player = players
+
+	// TODO: fill in result.discs
+	// TODO: fill in result.failed
+	// TODO: fill in result.points
+	// TODO: fill in result.time
+}
+
+func runTournament(commands []string, rounds int) : results []Result {
+	/*
+	for r := 0; r < rounds; r++ {
+		fmt.Println("Round", r + 1, "of", rounds)
+		for i, a := range(players) {
+			for j, b := range(players) {
+				if i != j {
+					fmt.Println(a, "vs", b)
+				}
+			}
+		}
+	}
+	*/
+	results = []Result{runMatch(commands, int[2]{0, 1})}
+}
 
 func main() {
 	rounds := 1
@@ -18,9 +52,12 @@ func main() {
 	} else if rounds < 1 {
 		fmt.Fprintln(os.Stderr, "Invalid number of rounds passed!")
 	} else {
+		players := flag.Args()
+		runTournament(players, rounds)
 		// tournament runs here.
 		// - print ordered ranking with CodeCup total game points,
-		//   time used, and number of failures
+		//   games won, games tied, games lost, number of failures,
+		//   average time used
 		// - print win/draw/loss matrix
 		// - print average disk difference matrix
 	}
